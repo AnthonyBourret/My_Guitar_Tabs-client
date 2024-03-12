2b74c06798f0e3d8b861fa89383deb2f89b24a43
Anthony Bourret <anthony.bourret@gmail.com>
2024-03-12 21:52:41 +0100

    :hammer: HomePage CSS Improved

--- src/components/CustomComponents/SongCard.tsx
+++ src/components/CustomComponents/SongCard.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { SongCardProps } from "../../types/types";
 import BadgeStyle from "./BadgeStyle";
-import { IconInProgress, IconLearned, IconToLearn } from '../../svg';
 import capitalize from "../../utils/capitalizeFirstLetter";
 
 function SongCard({
@@ -12,21 +11,21 @@ function SongCard({
     Styles,
     status }: SongCardProps) {
     return (
-        <div className="flex flex-col gap-8 border border-primary bg-base-100 rounded-box w-full items-center p-4 min-[820px]:w-full shadow-xl">
-            <div className="w-full flex flex-col gap-2">
+        <div className="flex flex-col gap-8 border border-primary bg-base-100 rounded-box w-full items-center p-4 min-[400px]:w-full shadow-xl">
+            <div className="w-full flex flex-col items-center gap-4 min-[400px]:items-start min-[400px]:flex-row min-[400px]:justify-between">
 
-                <div className="flex justify-between">
-                    <h1 className="text-xl self-start font-semibold min-[820px]:text-2xl mr-4 min-[820px]:mr-6">{capitalize(title)}</h1>
-                    <div className="relative">
-                        {status === "In progress" && <IconInProgress />}
-                        {status === "To learn" && <IconToLearn />}
-                        {status === "Learned" && <IconLearned />}
-                    </div>
+                <div className="flex flex-col gap-1 items-center">
+                    <h1 className="text-2xl font-semibold min-[400px]:text-2xl min-[400px]:mr-6 min-[400px]:self-start">{capitalize(title)}</h1>
+                    <h2 className="text-lg min-[400px]:ml-14 min-[400px]:self-start min-[820px]:text-xl">By {artist}</h2>
                 </div>
-                <h2 className="text-lg self-start ml-8 min-[820px]:ml-14 min-[820px]:text-lg">By {artist}</h2>
+                <div className="m-2">
+                    {status === "In progress" && <div className="text-base font-semibold">In progress</div>}
+                    {status === "To learn" && <div className="text-base font-semibold">To learn</div>}
+                    {status === "Learned" && <div className="text-base font-semibold">Learned</div>}
+                </div>
             </div>
 
-            <div className="w-full flex flex-col gap-8 items-center min-[820px]:flex-row min-[820px]:justify-between">
+            <div className="w-full flex flex-col gap-8 items-center min-[400px]:flex-row min-[400px]:justify-between">
                 <div className="flex gap-4 justify-center">
                     {Styles.map((style: { id: number, name: string }) => (
                         <BadgeStyle key={style.id} style={style.name} />
@@ -35,7 +34,7 @@ function SongCard({
                 <Link
                     to={`/song/${id}`}
 
-                    className="btn btn-sm w-fit btn-primary min-[820px]:btn-md"
+                    className="btn btn-md w-fit btn-primary min-[400px]:btn-md"
                 >
                     See Tabs
                 </Link>
--- src/svg/index.tsx
+++ src/svg/index.tsx
@@ -1,7 +1,4 @@
 export { default as Logo } from './Logo';
-export { default as IconToLearn } from './IconToLearn';
-export { default as IconInProgress } from './IconInProgress';
-export { default as IconLearned } from './IconLearned';
 export { default as IconDelete } from './IconDelete';
 export { default as IconLink } from './IconLink';
 export { default as IconEdit } from './IconEdit';
